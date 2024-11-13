e9f94845d1b7d3c68288d0e01b596dfa8a988f52
Nazanin <roozbahani.n92@gmail.com>
2024-11-14 01:12:56 +0330

    feat: implement filter and sort buttons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,12 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        greyishbrown: {
+          900: "#464646",
+        },
+      },
+      fontFamily: {
+        iransans: ["var(--font-iransans)"],
       },
     },
   },
---
+++ utils/translations.ts
@@ -0,0 +1,7 @@
+export const translations = {
+  filtering: "فیلتر کردن",
+  sorting: "مرتب‌سازی",
+  tehranToIstanbul: "بلیط هواپیمای تهران به استانبول",
+  flightsFound: "۲۷ پرواز یافت شد .",
+  departureDateTime: "سه‌شنبه، ۱۲ اردیبهشت ۱۴۰۰",
+};
